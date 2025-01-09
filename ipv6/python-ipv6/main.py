a772119ad067bf85ccfe7945f6a5bbefde7642ea
Philipp Tiesel <philipp.tiesel@sap.com>
2025-01-09 13:48:50 +0100

    Removed h2 for now for straight forward ipv6 support

--- ipv6/python-ipv6/main.py
+++ ipv6/python-ipv6/main.py
@@ -1,83 +1,54 @@
-import socket
 import os
 import platform
 
-import h2.connection
-import h2.config
+from twisted.web import server
+from twisted.web.resource import Resource
+from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ServerEndpoint, TCP6ServerEndpoint 
 
 import requests
 
-# Example H2C web app
-# Based on https://python-hyper.org/projects/h2/en/stable/basic-usage.html#writing-your-server
+class Handler(Resource):
+    isLeaf = True
 
-
-def handle(sock):
-    config = h2.config.H2Configuration(client_side=False)
-    conn = h2.connection.H2Connection(config=config)
-    conn.initiate_connection()
-    sock.sendall(conn.data_to_send())
-
-    while True:
-        data = sock.recv(65535)
-        if not data:
-            break
-
-        events = conn.receive_data(data)
-        for event in events:
-            if isinstance(event, h2.events.RequestReceived):
-                bodytext = "Hello!\nThis Python application is speaking plain text HTTP2 (H2C) with the CF routing layer and supports IPv4 as well as IPv6\n"
-
-                match platform.system:
-                    case "Linux":
-                        bodytext += "running on Linux\n"
-                        fd = open('/proc/sys/net/ipv6/bindv6only', r)
-                        bindv6only = read(fd)
-                        close(fd)
-                        if bindv6only:
-                            bodytext += "IPv6 Wildcard sockets also bind on IPv4 by default\n"
-                        else:
-                            bodytext += "IPv6 Wildcard sockets do not bind on IPv4 by default - needs sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)\n"
-                    
-                try:
-                    rd = requests.get('http://ds.test-ipv6.com/ip/',   params={'callback': 'ds'},  timeout=0.20)
-                    bodytext += "callout-{}\n".format(rd.text.rstrip())
-                except requests.exceptions.RequestException as e:
-                    bodytext += "callout-ds({})\n".format(e)                
-                try:
-                    r4 = requests.get('http://ipv4.test-ipv6.com/ip/', params={'callback': 'ip4'}, timeout=0.20)
-                    bodytext += "callout-{}\n".format(r4.text.rstrip())
-                except requests.exceptions.RequestException as e:
-                    bodytext += "callout-ip4({})\n".format(e)
+    def render_GET(self, request):
                 
-                try: 
-                    r6 = requests.get('http://ipv6.test-ipv6.com/ip/', params={'callback': 'ip6'}, timeout=0.20)
-                    bodytext += "callout-{}\n".format(r6.text.rstrip())
-                except requests.exceptions.RequestException as e:
-                    bodytext += "callout-"+"ip6({})\n".format(e)
+        bodytext = "Hello!\nThis Python application supports IPv4 as well as IPv6\n"   
+        
+        match platform.system:
+            case "Linux":
+                bodytext += "running on Linux\n"
+                fd = open('/proc/sys/net/ipv6/bindv6only', r)
+                bindv6only = read(fd)
+                close(fd)
+                if bindv6only:
+                    bodytext += "IPv6 Wildcard sockets also bind on IPv4 by default - that does not help you in python\n"
+                else:
+                    bodytext += "IPv6 Wildcard sockets do not bind on IPv4 by default)\n"
 
-                stream_id = event.stream_id
-                conn.send_headers(
-                    stream_id=stream_id,
-                    headers=[
-                        (':status', '200'),
-                        ('content-type', 'text/plain')
-                    ],
-                )
-                conn.send_data(
-                    stream_id=stream_id,
-                    data=bodytext.encode(),
-                    end_stream=True
-                )
+        try:
+            rd = requests.get('http://ds.test-ipv6.com/ip/',   params={'callback': 'ds'},  timeout=0.20)
+            bodytext += "callout-{}\n".format(rd.text.rstrip())
+        except requests.exceptions.RequestException as e:
+            bodytext += "callout-ds({})\n".format(e)                
+        try:
+            r4 = requests.get('http://ipv4.test-ipv6.com/ip/', params={'callback': 'ip4'}, timeout=0.20)
+            bodytext += "callout-{}\n".format(r4.text.rstrip())
+        except requests.exceptions.RequestException as e:
+            bodytext += "callout-ip4({})\n".format(e)
 
-        data_to_send = conn.data_to_send()
-        if data_to_send:
-            sock.sendall(data_to_send)
+        try: 
+            r6 = requests.get('http://ipv6.test-ipv6.com/ip/', params={'callback': 'ip6'}, timeout=0.20)
+            bodytext += "callout-{}\n".format(r6.text.rstrip())
+        except requests.exceptions.RequestException as e:
+            bodytext += "callout-"+"ip6({})\n".format(e)
 
-sock = socket.socket(family=socket.AF_INET6)
-sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('::', int(os.getenv('PORT'))))
-sock.listen(5)
+        return(bodytext.encode('utf-8'))
 
-while True:
-    handle(sock.accept()[0])
+
+site = server.Site(Handler())
+endpoint4 = TCP4ServerEndpoint(reactor, int(os.getenv('PORT')))
+endpoint4.listen(site)
+endpoint6 = TCP6ServerEndpoint(reactor, int(os.getenv('PORT')))
+endpoint6.listen(site)
+reactor.run()
